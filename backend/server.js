dbdc2e128fa5c6639341e0f7a3255365aa4016ca
Ryan Puffer <ryanpuffer@ryans-mbp.attlocal.net>
2020-12-21 13:05:03 -0600

    add error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/error.middleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
