3c6b9721abbc8b7820cf38e3a49185178aa42f40
Ryan Puffer <ryanpuffer@ryans-mbp.attlocal.net>
2020-12-23 11:03:43 -0600

    add loader components/add productDetails reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
